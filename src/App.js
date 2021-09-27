da0cc0a203da3a1f75fbc83d87581a7fb0174ad7
Jack Browne <jackbrowne00@gmail.com>
2021-09-27 14:44:28 +0100

    react router setup begin

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.scss';
+import Nav from './components/Nav/Nav'
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import Projects from './pages/Projects/Projects';
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="top">
-        <h1>Hey i'm Jack</h1>
-        <h2>I'm a Front-End Software Engineer</h2>
+    <Router>
+      <div className="App">
+        <Nav />
+          <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/projects" component={Projects}/>
+            <Route path="/contact" component={Contact}/>
+          </Switch> 
       </div>
-
-      <div className="bottom">
-        <h1>Hey i'm Jack</h1>
-        <h2>I'm a Front-End Software Engineer</h2>
-      </div>
-    </div>
+    </Router>  
   );
 }
 
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../../App.scss'
+
+function Nav() {
+    return (
+        <nav>
+            <h3>Logo/Home</h3>
+            <ul>
+                <Link to='projects'>
+                    <li>Projects</li>
+                </Link>  
+                
+                <Link to='/contact'>
+                    <li>Contact</li>
+                </Link>  
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
